083400a9e3e2ea8d4b47edefd1d2627df4b4d56f
SvanLeeuwen5 <71378207+SvanLeeuwen5@users.noreply.github.com>
2022-01-26 16:33:33 +0100

    remove change

--- webapp.py
+++ webapp.py
@@ -1,50 +1,50 @@
 from flask import Flask, render_template, request, redirect, flash, current_app as app
 import sys, os, subprocess, time, json
-import pyBluezConnector
+#import pyBluezConnector
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/main/', methods = ['POST', 'GET'])
 def main():
-    init()
-    connect()
+    #init()
+    #connect()
     return render_template('main.html')
 
 @app.route('/connected/', methods = ['POST', 'GET'])
 def connectedDevices():
-#    filename = os.path.join(app.static_folder, 'message.json')
-#    with open(filename) as file:
-#        data = json.load(file)
-    with open("/home/pi/.config/meshctl/prov_db.json", 'r') as file:
+    filename = os.path.join(app.static_folder, 'message.json')
+    with open(filename) as file:
         data = json.load(file)
+    #with open("/home/pi/.config/meshctl/prov_db.json", 'r') as file:
+        #data = json.load(file)
     return render_template('connected.html', posts=data)
 
 @app.route('/on/<unicastAddress>', methods = ['GET'])
 def switchOnApp(unicastAddress):
     print("on : " + unicastAddress)
-    switchOn(unicastAddress)
+    #switchOn(unicastAddress)
     return redirect('/connected', code=302)
 
 @app.route('/off/<unicastAddress>', methods = ['GET'])
 def switchOffApp(unicastAddress):
     print("off : " + unicastAddress)
-    switchOff(unicastAddress)
+    #switchOff(unicastAddress)
     return redirect('/connected', code=302)
 
 @app.route('/status/<unicastAddress>', methods = ['GET'])
 def statusRequest(unicastAddress):
     #responseCode = "off"
-    responseCode = lightStat(unicastAddress)
+    #responseCode = lightStat(unicastAddress)
     return render_template('status.html', status = responseCode, uniAddress = unicastAddress)
 
 @app.route('/devices/add/<uidd>', methods = ['POST', 'GET'])
 def provision(uidd):
-    addDev(uidd)
-    #filename = os.path.join(app.static_folder, 'message.json')
-    #with open(filename) as file:
-    #    data = json.load(file)
-    with open("/home/pi/.config/meshctl/prov_db.json", 'r') as file:
+    #addDev(uidd)
+    filename = os.path.join(app.static_folder, 'message.json')
+    with open(filename) as file:
         data = json.load(file)
+    #with open("/home/pi/.config/meshctl/prov_db.json", 'r') as file:
+    #    data = json.load(file)
     return render_template('provision.html', uidd=uidd, posts=data)
 
 @app.route('/grouped/<uidd>', methods = ['POST', 'GET'])
@@ -52,7 +52,7 @@ def addToGroup(uidd):
     if request.method == 'POST':
         result = request.form
         deviceType = request.form['options']
-        group(result['uni'], result['group'], deviceType)
+        #group(result['uni'], result['group'], deviceType)
         return render_template("result.html", result = result, device = uidd)
 
 @app.route('/devices/', methods = ['POST', 'GET'])
@@ -62,9 +62,10 @@ def remove():
         data = json.load(file)
     return render_template('remove.html', posts=data)
 
-@app.route('/remove/<uidd>', methods = ['POST', 'GET'])
-def removeUidd(uidd):
-    removeDev(uidd)
+@app.route('/remove/<uidd>/<node0>/<node1>/<node2>/<node3>', methods = ['POST', 'GET'])
+def removeUidd(uidd, node0, node1, node2, node3):
+    #removeDev(uidd)
+    print(node0, node1, node2, node3)
     return redirect('/devices', code=302)
 
 if __name__ == '__main__':
