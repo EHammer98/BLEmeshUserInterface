2033bcec77c1cd85936e7a60a1f1bfb1dd2257ea
SvanLeeuwen5 <s.vanleeuwen5@student.avans.nl>
2022-01-27 10:27:37 +0100

    pi fix

--- webapp.py
+++ webapp.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, flash, current_app as app
 import sys, os, subprocess, time, json
+sys.path.insert(0, '/blemesh/')
 import pyBluezConnector
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/main/', methods = ['POST', 'GET'])
 def main():
-    init()
-    connect()
+    pyBluezConnector.init()
     return render_template('main.html')
 
 @app.route('/connected/', methods = ['POST', 'GET'])
@@ -21,23 +21,23 @@ def connectedDevices():
 
 @app.route('/on/<unicastAddress>', methods = ['GET'])
 def switchOnApp(unicastAddress):
-    switchOn(unicastAddress)
+    pyBluezConnector.switchOn(unicastAddress)
     return redirect('/connected', code=302)
 
 @app.route('/off/<unicastAddress>', methods = ['GET'])
 def switchOffApp(unicastAddress):
-    switchOff(unicastAddress)
+    pyBluezConnector.switchOff(unicastAddress)
     return redirect('/connected', code=302)
 
 @app.route('/status/<unicastAddress>', methods = ['GET'])
 def statusRequest(unicastAddress):
     #responseCode = "error"
-    responseCode = lightStat(unicastAddress)
+    responseCode = pyBluezConnector.lightStat(unicastAddress)
     return render_template('status.html', status = responseCode, uniAddress = unicastAddress)
 
 @app.route('/devices/add/<uidd>', methods = ['POST', 'GET'])
 def provision(uidd):
-    addDev(uidd)
+    pyBluezConnector.addDev(uidd)
     return render_template('provision.html', uidd=uidd)
 
 @app.route('/devices/config/<device>', methods = ['POST', 'GET'])
@@ -54,7 +54,7 @@ def addToGroup(device):
     if request.method == 'POST':
         result = request.form
         deviceType = request.form['options']
-        group(result['uni'], result['group'], deviceType)
+        pyBluezConnector.group(result['uni'], result['group'], deviceType)
         return render_template("result.html", result = result, device = device)
 
 @app.route('/devices/', methods = ['POST', 'GET'])
@@ -68,7 +68,7 @@ def remove():
 
 @app.route('/remove/<device>/<node0>/<node1>/<node2>/<node3>', methods = ['POST', 'GET'])
 def removeUidd(device, node0, node1, node2, node3):
-    removeDev(device, node0, node1, node2, node3)
+    pyBluezConnector.removeDev(device, node0, node1, node2, node3)
     return redirect('/devices', code=302)
 
 if __name__ == '__main__':
